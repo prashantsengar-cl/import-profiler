b12f65195acbca6df4871ae6e244543a87433bf0
prashantsengar-cl <92354548+prashantsengar-cl@users.noreply.github.com>
2024-10-18 18:26:34 +0530

    Update setup.py
    Can be installed as a package named "import_profiler3"

--- setup.py
+++ setup.py
@@ -7,13 +7,13 @@ with open("README.md", "rt") as fp:
 
 if __name__ == "__main__":
     setup(
-        name="import_profiler",
-        version="0.0.4.dev0",
-        author="David Cournapeau",
+        name="import_profiler3",
+        version="0.0.1",
+        author="David Cournapeau, Prashant Sengar",
         author_email="cournape@gmail.com",
         license="MIT",
         install_requires=["attrs >= 17.1.0", "tabulate >= 0.7.5"],
-        description="Import profiler to find bottlenecks in import times.",
+        description="Import profiler to find bottlenecks in import times - supports Python 3.",
         long_description=DESCRIPTION,
         py_modules=["import_profiler"],
     )
